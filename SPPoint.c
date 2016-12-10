2bb485234a84eeec789b3058e57f39a2414f5eab
Dor Duchovni <dor@FM0018.local>
2016-12-10 18:26:43 +0200

    added files

---
+++ SPPoint.c
@@ -0,0 +1,43 @@
+#include "SPPoint.h"
+#include <assert.h>
+#include <stdio.h>
+
+
+typedef struct sp_point_t {
+	int index;
+	int dim;
+	double* data;
+} SPPoint;
+
+
+SPPoint* spPointCreate(double* data, int dim, int index) {
+	SPPoint* p;
+	p->data = (double*)malloc(sizeof(double)*dim);
+	p->data = data;
+	p->dim = dim;
+	p->index = index;
+
+	return p;
+}
+
+
+SPPoint* spPointCopy(SPPoint* source) {
+	assert(source != NULL);
+	SPPoint* copy;
+	int i = 0;
+	copy->data = (double*)malloc(sizeof(double)*source->dim);
+	for(i = 0; i < source->dim; ++i) {
+		copy->data[i] = source->data[i];
+	}
+	copy->dim = source->dim;
+	copy->index = source->index;
+
+	return copy;
+}
+
+int main(int argc, char **argv) {
+	double arr[] = {1,2,3,4,5};
+	int size = 5;
+	int index = 2;
+	SPPoint* p = spPointCreate(&arr, size , index);
+}
